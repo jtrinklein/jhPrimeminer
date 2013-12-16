15743bb74fb666faf220f8ccf7cb54969aa4bcce
James Trinklein <jtrinklein@daptiv.com>
2013-12-16 09:48:41 -0800

    update deploy script

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install -y build-essential libssl-dev openssl git libgmp3-dev
 git clone https://github.com/jtrinklein/jhPrimeminer.git
-cd jhPrimeminer.git
+cd jhPrimeminer
 make
 screen -S miner -dm ./jhprimeminer -o http://ypool.net/ -u theotherjim.$1 -p $1
 
