853d3c725360b7459ca3a9dea70a740bc6da1103
James Trinklein <jtrinklein@daptiv.com>
2013-12-16 10:29:03 -0800

    more tweaks to deploy script

--- script.sh
+++ script.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+if [ -z "$1" ]
+then
+    echo forgot the worker name!
+    exit
+fi
 apt-get update
 apt-get install -y build-essential libssl-dev openssl git libgmp3-dev
 git clone https://github.com/jtrinklein/jhPrimeminer.git
 cd jhPrimeminer
 make
-screen -S miner -dm ./jhprimeminer -o http://ypool.net/ -u theotherjim.$1 -p $1
+screen -A -d -m -S miner ~/jhPrimeminer/jhprimeminer -o http://ypool.net:10034/ -u theotherjim.$1 -p $1
+status=(screen -ls | grep 'miner')
+if [ -z "$status" ]
+then
+    echo there was a problem starting the screen session
+else
+    echo screen session appears to have started successfully
+fi
 
