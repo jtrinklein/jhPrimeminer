92bfe38aca787abb78a82840ee2abbe41d4bcd24
James Trinklein <theotherjim@gmail.com>
2013-12-20 17:28:03 -0800

    update script to be reusable after initial install

--- script.sh
+++ script.sh
@@ -4,11 +4,17 @@ then
     echo forgot the worker name!
     exit
 fi
-apt-get update
-apt-get install -y build-essential libssl-dev openssl git libgmp3-dev
-git clone https://github.com/jtrinklein/jhPrimeminer.git
-cd jhPrimeminer
-make
+dir=$(ls ~ | grep 'jhPrimeminer')
+
+if [ -z "$dir" ]
+then
+    apt-get update
+    apt-get install -y build-essential libssl-dev openssl git libgmp3-dev
+    git clone https://github.com/jtrinklein/jhPrimeminer.git
+    cd jhPrimeminer
+    make
+fi
+
 screen -A -d -m -S miner ~/jhPrimeminer/jhprimeminer -o http://ypool.net:10034/ -u theotherjim.$1 -p $1
 status=(screen -ls | grep 'miner')
 if [ -z "$status" ]
